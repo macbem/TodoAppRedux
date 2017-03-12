f36d295a27ce90c369348dd1f8a4afe7a3735fdd
macbem <maciej.bembenista@gmail.com>
2017-03-12 22:53:56 +0100

    Load tests into describe() calls and fix the formatting

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,57 +1,36 @@
 import uuid from 'uuid';
 
-export const addTodo = text => {
-  return {
-    type: 'ADD_TODO',
-    todoId: uuid.v4(),
-    text
-  };
-};
+export const addTodo = text => ({
+  type: 'ADD_TODO',
+  todoId: uuid.v4(),
+  text
+});
 
-export const editTodo = (todoId, text) => {
-  return {
-    type: 'EDIT_TODO',
-    todoId,
-    text
-  };
-};
+export const editTodo = (todoId, text) => ({
+  type: 'EDIT_TODO',
+  todoId,
+  text
+});
 
-export const setEditingFlag = todoId => {
-  return {
-    type: 'SET_EDITING_FLAG',
-    todoId
-  };
-};
+export const removeTodo = todoId => ({
+  type: 'REMOVE_TODO',
+  todoId
+});
 
-export const removeTodo = todoId => {
-  return {
-    type: 'REMOVE_TODO',
-    todoId
-  };
-};
+export const toggleTodo = todoId => ({
+  type: 'TOGGLE_TODO',
+  todoId
+});
 
-export const toggleTodo = todoId => {
-  return {
-    type: 'TOGGLE_TODO',
-    todoId
-  };
-};
+export const finishAllTodos = () => ({
+  type: 'FINISH_ALL_TODOS'
+});
 
-export const finishAllTodos = () => {
-  return {
-    type: 'FINISH_ALL_TODOS'
-  };
-};
+export const clearCompletedTodos = () => ({
+  type: 'CLEAR_COMPLETED_TODOS'
+});
 
-export const clearCompletedTodos = () => {
-  return {
-    type: 'CLEAR_COMPLETED_TODOS'
-  };
-};
-
-export const switchFilter = filter => {
-  return {
-    type: 'SWITCH_FILTER',
-    filter
-  };
-};
+export const switchFilter = filter => ({
+  type: 'SWITCH_FILTER',
+  filter
+});
--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,24 +1,35 @@
-import { addTodo, toggleTodo, switchFilter } from './index';
+import { addTodo, toggleTodo, switchFilter, editTodo } from './index';
 
-test('Test if adding a todo creates an action object of a certain shape', () => {
-  expect(addTodo('hello')).toEqual(expect.objectContaining({
-      type: 'ADD_TODO',
-      todoId: expect.any(String),
-      text: 'hello'
-    })
-  );
-});
-
-test('Test if toggling a todo creates a proper action object', () => {
-  expect(toggleTodo('test-id')).toEqual({
-    type: 'TOGGLE_TODO',
-    todoId: 'test-id'
+describe('creates a proper action object', () => {
+  test('adding a todo', () => {
+    expect(addTodo('hello')).toEqual(
+      expect.objectContaining({
+        type: 'ADD_TODO',
+        todoId: expect.any(String),
+        text: 'hello'
+      })
+    );
   });
-});
 
-test('Test if setting a filter creates a proper action object', () => {
-  expect(switchFilter('all')).toEqual({
-    type: 'SWITCH_FILTER',
-    filter: 'all'
+  test('editing a todo', () => {
+    expect(editTodo('test-id', 'new text')).toEqual({
+      type: 'EDIT_TODO',
+      todoId: 'test-id',
+      text: 'new text'
+    });
+  });
+
+  test('toggling a todo', () => {
+    expect(toggleTodo('test-id')).toEqual({
+      type: 'TOGGLE_TODO',
+      todoId: 'test-id'
+    });
+  });
+
+  test('setting a filter', () => {
+    expect(switchFilter('all')).toEqual({
+      type: 'SWITCH_FILTER',
+      filter: 'all'
+    });
   });
 });
